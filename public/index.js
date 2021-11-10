0cb8a8abf9d0ffae4bcdacad744cd8b451024a20
cainan43 <40045303+cainang@users.noreply.github.com>
2021-11-10 17:34:35 -0300

    add project

---
+++ public/index.js
@@ -0,0 +1,46 @@
+function cadastrarQuest() {
+  var array = new Array($(".item_Errado")[0].value, $(".item_Errado")[1].value, $(".item_Errado")[2].value)
+  let dificuldade = $("#dificuldade option:selected").val();
+  let textencode = encodeURI(dificuldade);
+  let count = 0;
+  if ($(".Pergunta").val() == "" || $(".item_Correto").val() == "" || dificuldade == "") {
+    M.toast({ html: 'Pergunta ou item correto está vazio 😥' })
+    return;
+  }
+  array.map(item => {
+    if (item == "") {
+      count++;
+    }
+  })
+  if (count != 0) {
+    M.toast({ html: 'Item está vazio 😥' })
+    return;
+  }
+  const data = {
+    pergunta: $(".Pergunta").val(),
+    itens: array,
+    item_correto: $(".item_Correto").val()
+  }
+  axios({
+    method: "post",
+    url: 'https://classicdev-backend.herokuapp.com/create/quest?dificudade=' + textencode,
+    data: data,
+
+  }).then(function (response) {
+    M.toast({ html: 'Cadastrado com sucesso, Kazim e Luciano ❤' })
+    console.log(response);
+  })
+    .catch(function (error) {
+      M.toast({ html: 'Erro ao cadastrar 😥' })
+      console.log(error);
+    })
+}
+
+$(".btnCadastrar").on("click", () => {
+  cadastrarQuest();
+})
+
+$(document).ready(function () {
+  $('select').formSelect();
+
+});
